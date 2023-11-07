e97a2ed67dfda8885b634eae2ed63cda5cab12da
Aaron Dangc <adraging@gmail.com>
2023-11-06 19:13:25 -0800

    UI MENU AND JOVER SCENE

--- Brain_Rhapsody_Unity_Project/Assets/Scripts/Menu.cs
+++ Brain_Rhapsody_Unity_Project/Assets/Scripts/Menu.cs
@@ -10,16 +10,43 @@ public class Menu : MonoBehaviour
     public GameObject startButton;
     public GameObject creditButton;
     public GameObject quitButton;
+
+    // reference list sprites to change the button sprite
+    public List<Sprite> buttonSprites;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Menu Start");
     }
+    public void HoverStart(){
+        Debug.Log("Hover Start");
+    }
+
+    public void HoverStartExit(){
+        Debug.Log("Hover Start Exit");
+    }
 
     public void StartGame()
     {
+        // Getting the image game object
+        // GameObject imageGameObject = GameObject.Find("ButtonStartImage");
+        // Getting the image component
+        // Image imageComponent = imageGameObject.GetComponent<Image>();
+        // Changing the sprite
+        // imageComponent.sprite = buttonSprites[2];
         Debug.Log("Start Game");
+        // Change the sprite of the button to the pressed sprite
+
+        // load the game scene
         SceneManager.LoadScene("JO_Workspace");
+    }
+
+    public void HoverCredit(){
+        Debug.Log("Hover Credit");
+    }
+
+    public void HoverCreditExit(){
+        Debug.Log("Hover Credit Exit");
     }
 
     public void Credits()
@@ -28,16 +55,25 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene("Credit_Scene");
     }
 
+    public void HoverQuit(){
+        Debug.Log("Hover Quit");
+    }
+
+    public void HoverQuitExit(){
+        Debug.Log("Hover Quit Exit");
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit Game");
         Application.Quit();
     }
 
+    // Credit Scene - Back Button
     public void QuitMenu()
     {
         Debug.Log("Quit Menu");
-        SceneManager.LoadScene("Menu_Scene");
+        SceneManager.LoadScene("Menu");
     }
 
 
